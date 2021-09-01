acbed8924c51355f8f5246efe7773184f4277f6b
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-09-01 16:12:12 -0700

    finalcommit

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,8 +2,6 @@ const express = require('express');
 const { validateUserId, validateUser,validatePost } = require('../middleware/middleware')
 const User = require('./users-model')
 const Post = require('../posts/posts-model')
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
 
 const router = express.Router();
 
@@ -16,14 +14,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
   res.json(req.user)
 });
 
 router.post('/', validateUser, (req, res, next) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
   User.insert(req.body)
   .then(user => {
     res.status(201).json(user)
@@ -33,20 +27,27 @@ router.post('/', validateUser, (req, res, next) => {
   })
 });
 
-router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  User.update(req.params.id, req.body)
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(error => {
+    next(error)
+  })
 });
 
-router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+router.delete('/:id', validateUserId, (req, res, next) => {
+  User.remove(req.params.id)
+  .then(() => {
+    res.status(200).json(req.user)
+  })
+  .catch(error => {
+    next(error)
+  })
 });
 
 router.get('/:id/posts', validateUserId, (req, res, next) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
   User.getUserPosts(req.params.id)
   .then(post => {
     res.status(200).json(post)
@@ -57,9 +58,6 @@ router.get('/:id/posts', validateUserId, (req, res, next) => {
 });
 
 router.post('/:id/posts',validateUserId, validatePost, (req, res, next) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
   const postInfo = {...req.body, user_id: req.params.id}
 
   Post.insert(postInfo)
@@ -78,5 +76,4 @@ router.use((err, req, res, next) => {
   })
 })
 
-// do not forget to export the router
 module.exports = router
